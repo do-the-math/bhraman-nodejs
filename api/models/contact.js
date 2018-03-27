39cb43ca16f999bb2733cab3fb73ea8a340c299b
Aman Nidhi <aman.nidhi@ggktech.local>
2018-03-27 19:31:28 +0530

    added userID to services

--- api/models/contact.js
+++ api/models/contact.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const contactSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	name: String,
+	userID: String,
 	categoryID: String,
 	location: String,
 	date: String,
